52172d64a89cadfefabd23a349b0c87f12505aa1
Hianne Alves <hiannequeiroz@yahoo.com.br>
2025-08-29 01:33:03 -0300

    verificador de caracteres

---
+++ EX_BASICO_LEN_IN/BASICO02.py
@@ -0,0 +1,9 @@
+# VERIFICADOR DE CARACTERES
+# PEÇA AO USUARIO PARA DIGITAR UMA PALAVRA. VERIFIQUE E IMPRIMA SE A LETRA "A " ESTÁ 
+# PRESENTE NA PALAVRA.
+
+palavra=input("Digite uma palavra: ")
+if "a" in palavra:
+    print("A letra 'a' está presente na palavra")
+else:
+    print("Esta palavra não contém a letra 'a'.")    
