1ceb7d0b72a1ba0226ea9e5107125e959a2529b8
Hianne Alves <hiannequeiroz@yahoo.com.br>
2025-08-29 16:20:30 -0300

    substiruir caractere

---
+++ EX_BASICO_LEN_IN/EX_COMBINANDO_METODOS_DE_OGICA/EX_METODO_STRING/BASICO09.py
@@ -0,0 +1,8 @@
+# SUBSTITUIR CARACTERE
+# PEÇA AO USUÁRIO PARA DIGITAR UMA FRASE 
+# SUBISTITUA TODAS AS LETRAS "A", POR "O"
+# IMPRIMA O RESULTADO
+
+frase = input("Digite uma frase:").lower()
+frase_nova = frase.replace("a","o")
+print(frase_nova)
