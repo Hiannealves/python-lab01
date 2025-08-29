aa0306d3cb5290a3a4caf15754a27eba6128ec29
Hianne Alves <hiannequeiroz@yahoo.com.br>
2025-08-29 01:34:00 -0300

    encontra a primeira posição no indice.

---
+++ EX_BASICO_LEN_IN/BASICO04.py
@@ -0,0 +1,7 @@
+# ENCONTRA A PRIMEIRA POSIÇÃO
+# PEÇA AO USUARIO PARA DIGITAR UMA PALAVRA E E IMPRIMA A POSIÇÃO , INDICE,
+# DA PRIMEIRA LETRA "E" QUE ELE ENCONTRAR.
+
+palavra=input("Digite uma palavra")
+posição_palavra =palavra.index('e')
+print(posição_palavra)
